89f40750d90b8e722b449991eb57a50eb00c74f1
Zihang Chen <zchen097@connect.hkust-gz.edu.cn>
2025-10-28 21:08:05 +0100

    [Update script] collect_csv.py
    Author: Zihang Chen <zchen097@connect.hkust-gz.edu.cn>
    Date: 2025-10-28 21:08:05

--- scripts/collect_csv.py
+++ scripts/collect_csv.py
@@ -23,6 +23,7 @@ benchmarks = [
 ]
 # =============================================
 
+
 def collect_and_zip(base_dir, zips_dir, zip_prefix):
     csv_dir = os.path.join(base_dir, "csv")
     os.makedirs(csv_dir, exist_ok=True)
@@ -30,14 +31,17 @@ def collect_and_zip(base_dir, zips_dir, zip_prefix):
     for bench in benchmarks:
         metrics_path = os.path.join(base_dir, bench, "metrics.csv")
         if os.path.exists(metrics_path):
-            new_name = f"{bench}_metrics.csv"
+            new_name = f"{bench}.csv"
             target_path = os.path.join(csv_dir, new_name)
             shutil.copy(metrics_path, target_path)
             print(f"[OK] Copied: {metrics_path} → {target_path}")
         else:
             print(f"[Skip] {bench}: metrics.csv not found.")
 
-    shutil.copy(os.path.join(base_dir, "run_summary.log"), os.path.join(csv_dir, "run_summary.log"))
+    shutil.copy(
+        os.path.join(base_dir, "run_summary.log"),
+        os.path.join(csv_dir, "run_summary.log"),
+    )
 
     zip_name = f"{zip_prefix}.zip"
     zip_path = os.path.join(base_dir, zip_name)
@@ -56,10 +60,18 @@ def collect_and_zip(base_dir, zips_dir, zip_prefix):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Collect metrics.csv from benchmarks and zip them.")
-    parser.add_argument("--zip_prefix", help="Prefix name for the output zip file (without .zip)")
-    parser.add_argument("--input", required=True, help="Input directory containing benchmark folders")
-    parser.add_argument("--output", required=True, help="Output directory for the zip file")
+    parser = argparse.ArgumentParser(
+        description="Collect metrics.csv from benchmarks and zip them."
+    )
+    parser.add_argument(
+        "--zip_prefix", help="Prefix name for the output zip file (without .zip)"
+    )
+    parser.add_argument(
+        "--input", required=True, help="Input directory containing benchmark folders"
+    )
+    parser.add_argument(
+        "--output", required=True, help="Output directory for the zip file"
+    )
     args = parser.parse_args()
 
     collect_and_zip(args.input, args.output, args.zip_prefix)
